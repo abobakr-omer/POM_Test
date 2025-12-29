f19a510d83e5dbbb5eec8082828098aa3f76453e
20812017101274 <20812017101274@eng.zu.edu.eg>
2025-12-29 23:53:05 +0200

    Your commit message

--- src/main/java/CustomListeners/TestNGListeners.java
+++ src/main/java/CustomListeners/TestNGListeners.java
@@ -43,6 +43,7 @@ public class TestNGListeners implements IInvokedMethodListener, ITestListener, I
 
     public void onExecutionFinish() {
         System.out.println("Execution finished");
+        AllureUtils.setAllureEnvironment();
 
     }
 
--- src/main/java/utils/AllureUtils.java
+++ src/main/java/utils/AllureUtils.java
@@ -1,11 +1,13 @@
 package utils;
 
+import com.google.common.collect.ImmutableMap;
 import io.qameta.allure.Allure;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.nio.file.Path;
 
+import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 import static java.nio.file.Files.newInputStream;
 
 public class AllureUtils {
@@ -22,6 +24,19 @@ public class AllureUtils {
         } catch (Exception e) {
             System.out.println("Error attaching screenshot to Allure");
         }
+
+    }
+
+    public static void setAllureEnvironment() {
+        allureEnvironmentWriter(
+                ImmutableMap.<String, String>builder()
+                        .put("OS", PropertyReader.getProperty("os.name"))
+                        .put("JDK Version", PropertyReader.getProperty("java.runtime.version"))
+                        .put("URL", "https://www.saucedemo.com/v1/index.html")
+                        .build(), PropertyReader.getProperty("user.dir")
+                        + File.separator + "test-output/allure-results" + File.separator
+
+        );
 
     }
 
